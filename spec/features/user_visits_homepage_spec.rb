14b66aa681cc4e47344b6f83a89ae12a1611ce57
John El Basha <johnelbasha@Johns-MacBook-Pro.local>
2022-05-28 15:13:35 +0400

    Added rspec-rails gem and installed.

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.feature "User visits homepage" do
+  scenario "User navigates to base URL" do
+    visit "/"
+
+    expect(page).to have_text("Tumbleweed.")
+  end
+end
